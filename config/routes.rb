ae671e1f08b627a254cdc6f94b485f8a26559e24
kuroiwayuki <yuki8050@icloud.com>
2025-04-23 20:38:38 +0900

    最初のコミット

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'tasks/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root to: 'tasks#index'
+  resources :tasks
+end
