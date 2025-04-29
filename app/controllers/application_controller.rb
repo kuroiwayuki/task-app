13c4f25ab6a58f1c42084ef02af1f68b21ddbe08
kuroiwayuki <yuki8050@icloud.com>
2025-04-29 18:58:34 +0900

    タスクの期間選択#10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,15 @@ class ApplicationController < ActionController::Base
   private
 
   def set_current_user
-    if session[:user_id]
-      Current.user = User.find_by(id: session[:user_id])
-    end
+    return unless session[:user_id]
+
+    Current.user = User.find_by(id: session[:user_id])
+  end
+
+  def authenticate_user
+    return unless Current.user.nil?
+
+    flash[:notice] = 'ログインが必要です'
+    redirect_to('/')
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,19 @@
 class SessionsController < ApplicationController
-    def create
-      user = User.find_by(email: params[:email])
-        if user.present? && user.authenticate(params[:password])
-            session[:user_id] = user.id
-            flash[:success] = "ログインしました。"
-            redirect_to root_path
-        else
-            flash[:danger] = "ログイン失敗しました。もう一度試してください。"
-            render :new
-        end
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = 'ログインしました。'
+      redirect_to root_path
+    else
+      flash[:danger] = 'ログイン失敗しました。もう一度試してください。'
+      render :new
     end
+  end
 
-    def destroy
-        session[:user_id] = nil
-        flash[:success] = "ログアウトしました。"
-        redirect_to root_path
-    end
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'ログアウトしました。'
+    redirect_to root_path
+  end
 end
